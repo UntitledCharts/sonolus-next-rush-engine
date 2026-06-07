hyeon2006/sonolus-next-rush-plus-engine

feat: init sekai rush
feat: implement custom UI system and update core note logic

Comprehensive engine update for next sekai, including Custom UI elements.

1. New Custom UI System
- Added `sekai/play/custom_elements.py` to handle custom judgments and combo UI.
- Implemented `ComboJudge` to manage Fever logic and score calculation.
- Added `JudgmentAccuracy` (display ms offset) and `DamageFlash` effects.
- Created `sekai/lib/custom_elements.py` for shared drawing logic.

2. Note Logic & Performance
- Updated `note.py` with `tick_trigger` for complex Tick note validation.
- Optimized update loops by caching hitbox and layout data.
- Added support for `judgment_window_bad` in `buckets.py`.

3. Visual Effects
- Implemented `ParticleManager` for efficient lane effect rendering.
- Added visual feedback for 'Wrong Way' flick inputs.

chore: fix sonolus-py version

sekai/tutorial/framework.py
@@ -33,6 +33,13 @@ from sekai.lib.connector import (
     update_linear_connector_particle,
 )
 from sekai.lib.ease import EaseType, ease
+from sekai.lib.layer import (
+    LAYER_JUDGMENT_LINE,
+    LAYER_STAGE,
+    LAYER_STAGE_COVER,
+    LAYER_STAGE_LANE,
+    get_z,
+)
 from sekai.lib.layout import FlickDirection, transformed_vec_at
 from sekai.lib.note import (
     NoteKind,
@@ -64,7 +71,7 @@ class SlideEffectHandles(Record):
 def update_start():
     PhaseState.slide_is_active = False
     clear_instruction()
-    draw_stage()
+    draw_stage(get_z(LAYER_STAGE_LANE), get_z(LAYER_STAGE_COVER), get_z(LAYER_STAGE), get_z(LAYER_JUDGMENT_LINE))
     reset_zoom()
 
 

UntitledCharts/sonolus-next-rush-engine

sekai/tutorial/framework.py
@@ -33,6 +33,13 @@ from sekai.lib.connector import (
     update_linear_connector_particle,
 )
 from sekai.lib.ease import EaseType, ease
+from sekai.lib.layer import (
+    LAYER_JUDGMENT_LINE,
+    LAYER_STAGE,
+    LAYER_STAGE_COVER,
+    LAYER_STAGE_LANE,
+    get_z,
+)
 from sekai.lib.layout import FlickDirection, transformed_vec_at
 from sekai.lib.note import (
     NoteKind,
@@ -64,7 +71,7 @@ class SlideEffectHandles(Record):
 def update_start():
     PhaseState.slide_is_active = False
     clear_instruction()
-    draw_basic_stage()
+    draw_basic_stage(get_z(LAYER_STAGE_LANE), get_z(LAYER_STAGE_COVER), get_z(LAYER_STAGE), get_z(LAYER_JUDGMENT_LINE))
     reset_zoom()
 
 